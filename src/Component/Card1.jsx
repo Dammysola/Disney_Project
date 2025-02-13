61fd52960226502d86ac84bffa4ba1b3ca8cc470
Dammysola <dammyarowoshola@gmail.com>
2025-02-13 16:58:17 +0100

    update

--- src/Component/Card1.jsx
+++ src/Component/Card1.jsx
@@ -1,11 +1,11 @@
-
+import carouselImgae from "../assets/Images/first_img.jpeg"
 
 const Carousel = () => {
     return (
 
         
             <div id="carousel" >
-                <img id="carouselImage" src='./src/assets/Images/first_img.jpeg' alt="" />
+                <img id="carouselImage" src={carouselImgae} alt="" />
                 <div id="carouselTitle">
                     <h2 id="Carousel_header"> Three Disney & Pixar "tear-jerking Masterpieces" to be released in theaters</h2>
                     <h4 id="Carousel_text">Three masterpieces nominated for the Academy Award for Best Animated Feature are coming to the big screen</h4>
